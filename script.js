43612ec9e2e9ce0e2181313fb1629a7a100de7c0
Patrik Batista <pcr_batista@homail.com>
2021-06-10 11:02:11 -0300

    finalizar requisito 8

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-let blocks = document.getElementById('color-palette')
+let blocks = document.getElementById('color-palette');
+let corClick = "#000";
 blocks.addEventListener('click', function changeSelector(evento){
   document.getElementsByClassName('selected')[0].classList.remove('selected')
   evento.target.classList.add('selected');
+  let newColor = evento.target;
+  if(newColor === document.getElementById('black')){
+    corClick = "#000";
+  }else if(newColor === document.getElementById('red')){
+    corClick = "red";
+  }else if(newColor === document.getElementById('blue')){
+    corClick = "#0000ff";
+  }else if(newColor === document.getElementById('green')){
+    corClick = "green";
+  }
+})
+
+
+let pixelBlocks = document.getElementById('pixel-board');
+pixelBlocks.addEventListener('click', function pintandoPixel(bloco){
+  bloco.target.style.backgroundColor = corClick;
 })
